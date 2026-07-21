6c81268b19e5566b5b5f8134d7f05f9465bc360b
francesca marcia <ME\du_349141-1781865730@a-2rdhipfr6xjdo.me.ga.co>
2026-07-21 12:16:00 +0100

    added controller, added endpoint for get/push and comments

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv'; // Dotenv is a package that lets us use environment variables
 dotenv.config() // we call this to initialize dotenv
-
-import express from "express"; // we need express as it's the framework (package) to build API
+import express from "express"; //we need the express package because it provides the router object
+import Router from "./views/router.js"; // import the router object from the router file
 
 const port = process.env.PORT; // get the PORT variable from the .env file
 const app = express(); //create a new express application (use express.json)
 
 app.use(express.json());// this configures express to read request body ehrn a user sends data
+app.use(Router);
 
 async function startServer() { // we can make the code wait for someting with the async function, the async function can wait for things to happen
   try {
--- views/router.js
+++ views/router.js
@@ -1,17 +1,15 @@
-import express from "express"; //we need the express package because it provides the router object
+import express from "express";
+import accountsController from "../controllers/accountsController.js";
 
-const Router = express.Router(); //create a new router object
+const Router = express.Router();
 
-Router.route("/accounts") //define an endpointfor /accounts
-  .get((req, res) => { // we tell it what to do when a user sends a GET request to /account
-    res.send(
-      "🤖 Accounts Route with GET method - this endpoint will get all of the accounts from the database"
-    );
-  })
-  .post((req, res) => {// we tell it what to do when a user sends a POST request to /account
-    res.send(
-      "🤖 Accounts Route with POST method - this endpoint will create a new account in the database"
-    );
-  });
+Router.route("/accounts") //this creates an endpoint for /accounts
+  .get(accountsController.getAllAccounts) //if the user sends a GET request to /account, use this function
+  .post(accountsController.createAccount);//if the user sends a POST request to /account, use this functio
+
+Router.route("/accounts/:id")
+  .get(accountsController.getAccountById) //if the user sends a GET request to /account:id, use this function
+  .put(accountsController.updateAccountById)//if the user sends a PUT request to /account:id, use this function
+  .delete(accountsController.deleteAccountById);//if the user sends a DELETE request to /account:id, use this function
 
 export default Router;

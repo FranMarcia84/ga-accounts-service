e3cc227776a56da9801cd236683f77d9344bc016
francesca marcia <ME\du_349141-1781865730@a-2rdhipfr6xjdo.me.ga.co>
2026-07-21 10:33:39 +0100

    added the code to start the app with comments

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from 'dotenv'; // Dotenv is a package that lets us use environment variables
+dotenv.config() // we call this to initialize dotenv
 
-const port = process.env.PORT;
+import express from "express"; // we need express as it's the framework (package) to build API
 
-console.log(port);
+const port = process.env.PORT; // get the PORT variable from the .env file
+const app = express(); //create a new express application (use express.json)
+
+app.use(express.json());// this configures express to read request body ehrn a user sends data
+
+async function startServer() { // we can make the code wait for someting with the async function, the async function can wait for things to happen
+  try {
+    app.listen(port, () => console.log(`🤖 Listening on Port: ${port}`));
+  } catch (err) {
+    console.log("🤖 Oh no something went wrong", err);
+  }
+}
+
+startServer();
